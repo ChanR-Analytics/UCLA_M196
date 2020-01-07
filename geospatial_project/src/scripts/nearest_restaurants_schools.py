e60ec31824c1fbe72175ff00e4eaedde2544f0f7
TheChanRProject <garpchoo@gmail.com>
2020-01-07 11:03:21 -0800

    Committing

--- geospatial_project/src/scripts/nearest_restaurants_schools.py
+++ geospatial_project/src/scripts/nearest_restaurants_schools.py
@@ -21,10 +21,29 @@ for coord in coordinates:
     nearest_restaurant_results.append(search_result)
 
 
-# Creating a Data Frame of Results
-names = []
-latitudes = []
-longitudes = []
-total_user_ratings = []
-ratings = []
-nearest_restaurant_results[0]['results'][1]
+# Creating a Data Frame of Results for Each School
+frame_dict_list = []
+school_count = 0
+while school_count < len(nearest_restaurant_results):
+    names = []
+    latitudes = []
+    longitudes = []
+    total_user_ratings = []
+    ratings = []
+    for i in range(len(nearest_restaurant_results[school_count]['results'])):
+         name = nearest_restaurant_results[school_count]['results'][i]['name']
+         latitude = nearest_restaurant_results[school_count]['results'][i]['geometry']['location']['lat']
+         longitude = nearest_restaurant_results[school_count]['results'][i]['geometry']['location']['lng']
+         total_user_rating = nearest_restaurant_results[school_count]['results'][i]['user_ratings_total']
+         rating = nearest_restaurant_results[school_count]['results'][i]['rating']
+         names.append(name)
+         latitudes.append(latitude)
+         longitudes.append(longitude)
+         total_user_ratings.append(total_user_rating)
+         ratings.append(rating)
+
+    frame_dict = {'names': names, 'latitudes': latitudes, 'longitudes': longitudes, 'total_user_ratings': total_user_ratings, 'rating': ratings}
+    frame_dict_list.append(frame_dict)
+    school_count += 1
+
+frame_list = [pd.DataFrame.from_dict(frame_dict) for frame_dict in frame_dict_list]
