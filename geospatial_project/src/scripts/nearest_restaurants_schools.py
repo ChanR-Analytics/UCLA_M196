0f04473ad747a9933ff285724a0d18611909723a
TheChanRProject <garpchoo@gmail.com>
2020-01-07 09:24:47 -0800

    Committing

--- geospatial_project/src/scripts/nearest_restaurants_schools.py
+++ geospatial_project/src/scripts/nearest_restaurants_schools.py
@@ -2,3 +2,15 @@ import numpy as np
 import pandas as pd
 import googlemaps
 from os import getcwd, listdir
+from getpass import getpass
+
+# Getting Coordinates of Schools
+coord_path = getcwd() + "/geospatial_project/data/csv"
+coord_df = pd.read_csv(f"{coord_path}/{listdir(coord_path)[0]}")
+
+coordinates = list(zip(coord_df['latitude'].tolist(), coord_df['longitude'].tolist()))
+
+# Establishing Google Maps Client with API Key
+gmaps = googlemaps.Client(key=getpass("Type in your API key: "))
+
+# Search Restaurants Within a 10 Meter Radius from Each School
