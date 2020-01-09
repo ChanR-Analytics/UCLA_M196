80db9f9aeeb6b62eb45cd547ee692b02deb7087d
Rishov Chatterjee <garpchoo@gmail.com>
2020-01-09 15:17:17 -0800

    adding edits

--- geospatial_project/src/scripts/oop_test.py
+++ geospatial_project/src/scripts/oop_test.py
@@ -6,4 +6,12 @@ school_path = getcwd() + "/geospatial_project/data/csv"
 nr = nearest_restaurants(school_path)
 nr.view_schools()
 nr.make_coordinates()
-nr.search_results(query='restaurants', radius=10, now=True)
+nr_results = nr.search_results(query='restaurants', radius=10, now=True)
+
+nr_frame_dict = nr.frame_process(nr_results)
+
+nr_frame_dict['Arcadia High School'].head()
+
+haversine_results = nr.haversine_distance(nr_frame_dict)
+
+haversine_results['Arcadia High School']
