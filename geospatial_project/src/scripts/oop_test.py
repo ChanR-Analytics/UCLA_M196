42d3232333db959b3fd9e3c6a58d8ba302f262af
TheChanRProject <garpchoo@gmail.com>
2020-01-10 21:40:28 -0800

    Commiting my changes

--- geospatial_project/src/scripts/oop_test.py
+++ geospatial_project/src/scripts/oop_test.py
@@ -1,3 +1,4 @@
+from time import sleep
 from os import getcwd, listdir
 from geospatial_project.src.scripts.nearest_restaurants_schools_oop import nearest_restaurants
 from IPython.display import display
@@ -9,8 +10,24 @@ nr = nearest_restaurants(school_path)
 nr.view_schools()
 nr.make_coordinates()
 nr_results = nr.search_results(query='restaurants', radius=10, now=False)
+# Rather than Putting Page Result 2 and 3 in the Class, add them from the instantiated object
+count = 0
 
+school_names = nr.df['school_name'].tolist()
 
+result_dict = {}
+while count < len(nr_results):
+    result = nr_results[count]
+    sleep(3)
+    result2 = nr.gmaps.places(query='restaurants', page_token=result['next_page_token'])
+    sleep(3)
+    result3 = nr.gmaps.places(query='restaurants', page_token=result2['next_page_token'])
+    result_dict[school_names[count]] = [result, result2, result3]
+    count += 1
+
+# BOSS MAN 
+
+result_dict['Arcadia High School'][2]
 
 nr_results[0]['results'][0]
 
