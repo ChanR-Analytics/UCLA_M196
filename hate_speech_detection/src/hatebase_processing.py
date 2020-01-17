863363f2bfc14d6c92918e3d453ba9270e3ac27c
TheChanRProject <garpchoo@gmail.com>
2020-01-17 05:34:31 +0530

    Commiting my changes

--- hate_speech_detection/src/hatebase_processing.py
+++ hate_speech_detection/src/hatebase_processing.py
@@ -14,3 +14,41 @@ df.head()
 nltk.download('stopwords')
 
 stopwords = nltk.corpus.stopwords.words('english')
+
+other_exclusions = ["#ff", "ff", "rt"]
+stopwords.extend(other_exclusions)
+
+stemmer = PorterStemmer()
+
+def preprocess(text_string):
+    """
+    Accepts a text string and replaces:
+    1) urls with URLHERE
+    2) lots of whitespace with one instance
+    3) mentions with MENTIONHERE
+    This allows us to get standardized counts of urls and mentions
+    Without caring about specific people mentioned
+    """
+    space_pattern = '\s+'
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+        '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    parsed_text = re.sub(space_pattern, ' ', text_string)
+    parsed_text = re.sub(giant_url_regex, 'URLHERE', parsed_text)
+    parsed_text = re.sub(mention_regex, 'MENTIONHERE', parsed_text)
+    #parsed_text = parsed_text.code("utf-8", errors='ignore')
+    return parsed_text
+
+
+
+tweets = df['tweet'].tolist()
+
+def tokenize(tweet):
+    """Removes punctuation & excess whitespace, sets to lowercase,
+    and stems tweets. Returns a list of stemmed tokens."""
+    tweet = " ".join(re.split("[^a-zA-Z]*", tweet.lower())).strip()
+    #tokens = re.split("[^a-zA-Z]*", tweet.lower())
+    tokens = [stemmer.stem(t) for t in tweet.split()]
+    return tokens
+
+tokenize(tweets[0])
