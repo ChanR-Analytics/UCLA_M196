55f8e685738c942c6bf2bd6404f881da7ae20561
TheChanRProject <garpchoo@gmail.com>
2020-01-28 21:27:38 -0800

    Committing

--- hate_speech_detection/src/python/hate_speech_oop_test.py
+++ hate_speech_detection/src/python/hate_speech_oop_test.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from os import getcwd, listdir
 from hate_speech_detection.src.python.hate_speech_oop import HatebaseTwitter
 
@@ -13,3 +14,14 @@ hb.eda()
 
 # Extracting the Tweet TF-IDF, POS TF-IDF, and Other Tweet Features into a Multidimensional Data Matrix
 features = hb.features()
+
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from xgboost import XGBClassifier
+from ngboost import ngboost
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+X = pd.DataFrame(features)
+X.shape 
