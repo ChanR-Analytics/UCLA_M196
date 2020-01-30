0a24e39c6f8b08c472b4a2c1ab30413ec3e49e50
TheChanRProject <garpchoo@gmail.com>
2020-01-30 02:26:47 -0800

    Commiting my changes

--- hate_speech_detection/src/python/hate_speech_oop_test.py
+++ hate_speech_detection/src/python/hate_speech_oop_test.py
@@ -15,6 +15,7 @@ hb.eda()
 # Extracting the Tweet TF-IDF, POS TF-IDF, and Other Tweet Features into a Multidimensional Data Matrix
 features = hb.features()
 
+# Performing Machine Learning Using Dimensionality Reduction Techniques and Supervised Classification Techniques 
 X_ = hb.l1_dim_reduce(features)
 X_ = hb.rfe_dim_reduce(X_, 'ada', 20)
 multi_classifier = hb.classify(X_, 'binary', 'xgb', 0.15, res=True, res_method='up')
