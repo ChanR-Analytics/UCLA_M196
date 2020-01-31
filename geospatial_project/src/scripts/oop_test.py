9b46bf70a2dd5b7a98b40e3148099b4de87744cf
TheChanRProject <garpchoo@gmail.com>
2020-01-31 01:27:07 -0800

    Commiting my changes

--- geospatial_project/src/scripts/oop_test.py
+++ geospatial_project/src/scripts/oop_test.py
@@ -3,13 +3,17 @@ from os import getcwd, listdir
 from geospatial_project.src.scripts.nearest_restaurants_schools_oop import nearest_restaurants
 from IPython.display import display
 
-school_path = getcwd() + "/geospatial_project/data/csv"
+school_path = getcwd() + "/geospatial_project/data/csv/school_coordinates.csv"
 
 nr = nearest_restaurants(school_path)
 nr.view_schools()
 nr.make_coordinates()
 
 nr_results = nr.search_results(query='restaurants', radius=10, now=False)
+
+nr_results[0]
+
+
 # Rather than Putting Page Result 2 and 3 in the Class, add them from the instantiated object
 count = 0
 
@@ -27,12 +31,14 @@ while count < len(nr_results):
 
 nr_frame_dict = nr.frame_process(result_dict)
 
+nr_frame_dict['Granada Hills Charter High School'].head()
+
 haversine_results = nr.haversine_distance(nr_frame_dict, 'km')
 
 google_results = nr.google_distance(nr_frame_dict, transporation_mode='walking')
-
-for school in haversine_results.keys():
-    display(haversine_results[school].dropna().reset_index().drop('index', axis=1))
+google_results.keys()
+for school in google_results.keys():
+    display(google_results[school].dropna().reset_index().drop('index', axis=1))
 
 
 merged_df_results = nr.merge_frames(nr_frame_dict, haversine_results, google_results)
